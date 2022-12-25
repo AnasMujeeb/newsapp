a78e513e4863b7645fc30c9fb510dc5eb0be17c3
AnasMujeeb <anasmujeebuddin@gmail.com>
2022-12-25 16:27:00 +0500

    added features

--- src/Component/News.jsx
+++ src/Component/News.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Loader from './Loader.jsx';
 import NewsItem from "./NewsItem.jsx"
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 export default class News extends Component {
   
@@ -9,7 +10,8 @@ export default class News extends Component {
     this.state = {
       articles: [],
       loading: false,
-      page: 1
+      page: 1,
+      totalResult : 0
     }
   }
   async componentDidMount() {
@@ -19,43 +21,53 @@ export default class News extends Component {
     })
     const data = await fetch(url)
     const parsedData = await data.json()
-    console.log(parsedData)
-    this.setState({
-      articles: parsedData.articles,
-      loading: false
-    })
-  }
- 
-  async preBtnHandler(){
-    const url = `https://newsapi.org/v2/top-headlines?country=us&category=${this.props.category}&apiKey=e1d5f921911a46f48022936125f06baa&page=${this.state.page-1}&pageSize=9`
-    this.setState({
-      loading :true
-    })
-    const data = await fetch(url)
-    const parsedData = await data.json()
-    this.setState ({
-      articles : parsedData.articles,
-      loading : false,
-      page : this.state.page - 1
-    })
-  }
-  async nextBtnHandler(){
-    const url = `https://newsapi.org/v2/top-headlines?country=us&category=${this.props.category}&apiKey=e1d5f921911a46f48022936125f06baa&page=${this.state.page + 1}&pageSize=9`
-    this.setState({
-      loading :true
-    })
-
-    const data = await fetch(url)
-    const parsedData = await data.json()
-
+    console.log(parsedData.articles.length)
     this.setState({
       articles: parsedData.articles,
       loading: false,
+      totalResult : parsedData.totalResult
+    })
+  }
+ 
+  // async preBtnHandler(){
+  //   const url = `https://newsapi.org/v2/top-headlines?country=us&category=${this.props.category}&apiKey=e1d5f921911a46f48022936125f06baa&page=${this.state.page-1}&pageSize=9`
+  //   this.setState({
+  //     loading :true
+  //   })
+  //   const data = await fetch(url)
+  //   const parsedData = await data.json()
+  //   this.setState ({
+  //     articles : parsedData.articles,
+  //     loading : false,
+  //     page : this.state.page - 1
+  //   })
+  // }
+  // async nextBtnHandler(){
+  //   const url = `https://newsapi.org/v2/top-headlines?country=us&category=${this.props.category}&apiKey=e1d5f921911a46f48022936125f06baa&page=${this.state.page + 1}&pageSize=9`
+  //   this.setState({
+  //     loading :true
+  //   })
+
+  //   const data = await fetch(url)
+  //   const parsedData = await data.json()
+    
+  //   this.setState({
+  //     articles: parsedData.articles,
+  //     loading: false,
+  //     page : this.state.page + 1
+  //   })
+  // }
+  async fetchMoreData(){
+    this.setState({
       page : this.state.page + 1
     })
-  
-   
-    
+    const url = `https://newsapi.org/v2/top-headlines?country=us&category=General&apiKey=e1d5f921911a46f48022936125f06baa&page=${this.state.page}&pageSize=9`
+    const data = await fetch(url)
+    const newData = await data.json()
+    console.log(newData.articles)
+    this.setState({
+      articles: this.state.articles.concat(newData.articles),
+    })
   }
 
   render() {
@@ -69,12 +81,19 @@ export default class News extends Component {
         : <>
         <div className='container my-4'>
         <h1>News App</h1>
+          <InfiniteScroll 
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={true}
+          loader={<h4 className='container text-center'>Loading...</h4>}
+          >
         <div className='d-flex flex-row justify-content-around flex-wrap'>
           {this.state.articles.map(obj => (
             <NewsItem name={obj.title} description={obj.description} url={obj.url} urlToImage={obj.urlToImage} />
-          ))
+            ))
           }
         </div>
+          </InfiniteScroll>
         <div className='d-flex justify-content-between '>
 
         <button disabled={this.state.page <= 1 ? true : false} type="button" class="btn btn-dark" onClick={()=>this.preBtnHandler()}>&larr; Previous</button>
